a45f1e33bf88740613d138ee4ec81ba62cea70de
Josh Archer <jarcher@greenriver.edu>
2020-04-08 19:32:21 -0700

    Adding the initial files for the example.

---
+++ scripts/dice.js
@@ -0,0 +1,11 @@
+let button = document.querySelector("button");
+button.onclick = function(event) {
+    //stop the form from submitting
+    event.preventDefault();
+
+    //get our DOM elements
+
+    //write a loop that adds rolled dice until
+    //the target number appears
+
+};
